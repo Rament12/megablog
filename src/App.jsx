0827bbe877f5dcbf728637486aef809e0944d4db
Rament12 <ramentkathariya@gmail.com>
2025-08-30 00:58:22 +0545

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
+import { useEffect, useState } from 'react';
+import authService from './appwrite/auth';
+import { useDispatch } from 'react-redux';
+import { login, logout } from './store/authSlice';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({userData})); // or { userData } based on your reducer
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
 
-
-  return (
-    <>
-      <h1 className='bg-blue-500'>Blog app </h1>
-    </>
-  )
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+         Todo: {/* <Outlet /> if using React Router */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
-export default App
+export default App;
--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,57 +1,73 @@
 import config from "../config/config.js";
-import { Client, Account,ID } from "appwrite";
+import { Client, Account, ID } from "appwrite";
 
+class AuthService {
+  client = new Client();
+  account;
 
-export class AuthService{
-    client=new Client();
-    account;
-    constructor(){
-        this.client
-        .setEndpoint(config.VITE_APPWRITE_URL) // Your API Endpoint
-        .setProject(config.VITE_APPWRITE_PROJECT_ID); // Your project ID
-        this.account=new Account(this.client);
-    }
-    async createAccount({email,password,name}){
-        try{
-            const userAccount=await this.account.create(ID.unique(),email,password,name);
-            if(userAccount){
-                //call another method
-                return this.login({email,password});
-            }else{
-                return userAccount; 
-            }
-    }catch(err){
-        console.log(err);
-        throw err;
+  constructor() {
+    this.client
+      .setEndpoint(config.VITE_APPWRITE_URL) // Your API Endpoint
+      .setProject(config.VITE_APPWRITE_PROJECT_ID); // Your project ID
+
+    this.account = new Account(this.client);
+  }
+
+  // Create account and auto-login
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+
+      if (userAccount) {
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (err) {
+      console.error("createAccount error:", err);
+      throw err;
     }
   }
-    async login({email,password}){
-        try{
-            return await this.account.createEmailPasswordSession(email,password);
-        }catch(err){
-            console.log(err);
-            throw err;
-        }
+
+  // Login
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (err) {
+      console.error("login error:", err);
+      throw err;
     }
-    async getUserDetails(){
-        try{
-             await this.account.get();
-        }catch(err){
-            console.log("get user details error",err);
-        }
-        return null;
+  }
+
+  // Get current logged-in user
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (err) {
+      // This will happen if the user is not logged in
+      if (err.code === 401) {
+        console.warn("User not logged in.");
+      } else {
+        console.error("getCurrentUser error:", err);
+      }
+      return null;
     }
-    async logout(){
-        try{
-             await this.account.deleteSessions();
-        }catch(err){
-            console.log("logout error",err);
-            
-        }
+  }
+
+  // Logout all sessions
+  async logout() {
+    try {
+      await this.account.deleteSessions(); // deletes all sessions
+    } catch (err) {
+      console.error("logout error:", err);
     }
+  }
 }
 
-
-const authService=new AuthService();
-
+const authService = new AuthService();
 export default authService;
