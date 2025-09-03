e1cdbf3ca99b986c3e69f2d27921059bfb00c96f
Rament12 <ramentkathariya@gmail.com>
2025-09-04 02:00:31 +0545

    project

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,4 +1,4 @@
-import config from "../config/config.js";
+import config from "../conf/config.js";
 import { Client, Account, ID } from "appwrite";
 
 class AuthService {
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,82 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './components/pages/Home.jsx'
+import { AuthLayout, Login } from './components/index.js'
+import AddPost from "./components/pages/AddPost";
+import Signup from './components/pages/Signup'
+import EditPost from "./components/pages/EditPost";
+
+import Post from "./components/pages/Post";
+
+import AllPosts from "./components/pages/AllPosts";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+        {
+            path: "/",
+            element: <Home />,
+        },
+        {
+            path: "/login",
+            element: (
+                <AuthLayout authentication={false}>
+                    <Login />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/signup",
+            element: (
+                <AuthLayout authentication={false}>
+                    <Signup />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/all-posts",
+            element: (
+                <AuthLayout authentication>
+                    {" "}
+                    <AllPosts />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/add-post",
+            element: (
+                <AuthLayout authentication>
+                    {" "}
+                    <AddPost />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/edit-post/:slug",
+            element: (
+                <AuthLayout authentication>
+                    {" "}
+                    <EditPost />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/post/:slug",
+            element: <Post />,
+        },
+    ],
+},
+])
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <RouterProvider router={router} />
     </Provider>
-  </StrictMode>,
+  </React.StrictMode>,
 )
